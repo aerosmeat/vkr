7f6cd8db514f81e383391b9a5303e3e60aa164f3
aerosmeat <aerosmeat@gmail.com>
2016-03-02 20:03:20 +0300

    Update vkr.cpp

--- vkr.cpp
+++ vkr.cpp
@@ -1,30 +1,39 @@
 #include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "opencv2/opencv.hpp"
 #include <opencv\highgui.h>
+
+#define forest_cols 132
+#define forest_rows 63
 
 using namespace cv;
 using namespace std;
 
-int main() {
-	Mat img = imread("G:\\STUDY\\ÄÈÏËÎÌ\\examples\\1.jpg", 0);
-
+void create_sample(Mat image, Mat arr[forest_cols][forest_rows], const int cols, const int rows) {
 	int rshift = 0, dshift = 0;
-	//const int str = 63, stolb = 132;
-	const int str = 3, stolb = 5;
-	Mat sample[stolb][str];
-	for (int i = 0; i < stolb; i++) {
-		for (int j = 0; j < str; j++) {
-			Rect r(0 + rshift, 0 + dshift, 20, 20); //vydelyaem nyzhnyi fragment
-			Mat copy = img;
+
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			Rect r(0 + rshift, 0 + dshift, 20, 20);
+			Mat copy = image;
 			Mat crop = copy(r);
-			sample[i][j] = crop; //zapisivaem fragment v massiv
+			arr[i][j] = crop;
 			dshift += 10;
-			if (j == str) dshift = 0; 
+			if (j == rows - 1) dshift = 0;
 		}
 		rshift += 10;
 	}
+}
+
+int main() {
+	Mat img = imread("G:\\STUDY\\Ğ”Ğ˜ĞŸĞ›ĞĞœ\\examples\\1.jpg", 0);
+	Mat forest[forest_cols][forest_rows];
+	create_sample(img, forest, forest_cols, forest_rows);
+		
+	
 
 	namedWindow("Display window", CV_WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", sample[0][0]);
+	imshow("Display window", forest[0][0]);
 	waitKey(0);
 }
